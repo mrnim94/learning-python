011f434b4a4da7898373a820fd582c0a26d04080
mr nim <mrnim@nimtechnology.com>
2022-09-15 00:20:45 +0700

    this first time to learing Python

---
+++ README.md
@@ -0,0 +1,4 @@
+Bài 1. Cơ bản về Python (Phần 1/2)
+
+https://www.youtube.com/watch?v=TY43q0w1Eh0&list=PLZPCoTKpEddAay-lItE-pn27uNNrApORH&index=1
+
---
+++ bai1.py
@@ -0,0 +1,11 @@
+bien_1 = 0.1
+print('Biến 1 có giá trị là:',bien_1)
+print('Biến 1 có kiểu là là:',type(bien_1))
+
+chu = input("Bạn cần nhập chữ j đây?")
+print(chu)
+bien_2 = 5
+if bien_1 < bien_2:
+    print("Biến 1 nhỏ hơn biến 2")
+else:
+    print("Biến 1 lón hơn biến 2")
